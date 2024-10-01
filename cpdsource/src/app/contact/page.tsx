0997ff1609cc159bfac408015641d2616cee4dcd
Danie Otto <https://github.com/DanieOtto>
2024-10-01 21:36:39 +0200

    Updated contact details

--- cpdsource/src/app/contact/page.tsx
+++ cpdsource/src/app/contact/page.tsx
@@ -3,7 +3,7 @@ export default function Contact() {
       <div>
         <h1>Contact Us</h1>
         <p>Email: contact@yourcompany.com</p>
-        <p>Phone: 123-456-7890</p>
+        <p>Phone: 123-456-7891</p>
       </div>
     );
   }
