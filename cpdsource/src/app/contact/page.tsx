7569e41b1b0e32e80b58d4f2ed827d1152fdd283
Danie Otto <danieotto@live.com>
2024-10-06 08:58:01 +0200

    Updated contact details

--- cpdsource/src/app/contact/page.tsx
+++ cpdsource/src/app/contact/page.tsx
@@ -3,7 +3,7 @@ export default function Contact() {
       <div>
         <h1>Contact Us</h1>
         <p>Email: contact@yourcompany.com</p>
-        <p>Phone: 123-456-7891</p>
+        <p>Phone: 123-456-1234</p>
       </div>
     );
   }
